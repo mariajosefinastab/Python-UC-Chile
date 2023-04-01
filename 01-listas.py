daadd371a271f21a94a191282cd0856fd02ad8b7
Maria Josefina Stabile <mariajosefinastabile@gmail.com>
2023-04-01 08:41:20 -0300

    first commit

---
+++ 01-listas.py
@@ -0,0 +1,6 @@
+salidas = [['00:05', 'LA622', 'Ciudad de México', 'E12', 'Cancelado'],
+           ['00:49', 'LA578', 'Bogota', 'E09', 'Atrasado']]
+
+for fila in salidas:
+    for elem in fila:
+        print(elem)
